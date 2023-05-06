86c765a95ead45a990248f185385cc2f82789b09
loga.vanan <loga.vanan@mahiti.org>
2023-05-06 15:26:25 +0530

    bugs

--- health_management/management/commands/create_clinic_data.py
+++ health_management/management/commands/create_clinic_data.py
@@ -32,25 +32,27 @@ class Command(BaseCommand):
                     is_alcoholic=cd.alcohol, is_smoker=cd.smoking)
                     treatment_obj.save()
                 if cd.htn:
-                    # diagnosis_details_tn = Diagnosis.objects.filter(patient_uuid=patient_uuid, 
-                    # ndc_id=cd.htn, server_created_on__date='2023-05-06').exists()
-                    # if not diagnosis_details_tn:
-                    diagnosis_th_obj = Diagnosis.objects.create(uuid=uuid_id,
-                    user_uuid=user_uuid, patient_uuid=patient_uuid, ndc_id=cd.htn, 
-                    source_treatment=cd.source_treatment, detected_by=cd.detected_by_htn, 
-                    years=cd.detected_since_htn, server_created_on=cd.visit_date)
-                    diagnosis_th_obj.save()
-                    print('TN SERIES')
+                    diagnosis_details_tn = Diagnosis.objects.filter(patient_uuid=patient_uuid, 
+                    ndc_id=cd.htn, server_created_on__date=cd.visit_date).exists()
+                    if not diagnosis_details_tn:
+                        diagnosis_th_obj = Diagnosis.objects.create(uuid=uuid_id,
+                        user_uuid=user_uuid, patient_uuid=patient_uuid, ndc_id=cd.htn, 
+                        source_treatment=cd.source_treatment, detected_by=cd.detected_by_htn, 
+                        years=cd.detected_since_htn)
+                        diagnosis_th_obj.server_created_on=cd.visit_date
+                        diagnosis_th_obj.save()
+                        print('TN SERIES')
                 if cd.dm:
-                    # diagnosis_details_dm = Diagnosis.objects.filter(patient_uuid=patient_uuid,
-                    # ndc_id=cd.dm, server_created_on__date='2023-05-06').exists()
-                    # if not diagnosis_details_dm:
-                    diagnosis_dm_obj = Diagnosis.objects.create(uuid=uuid_id,
-                    user_uuid=user_uuid, patient_uuid=patient_uuid, ndc_id=cd.dm, 
-                    source_treatment=cd.source_treatment, detected_by=cd.detected_by_dm, 
-                    years=cd.detected_since_dm, server_created_on=cd.visit_date)
-                    diagnosis_dm_obj.save()
-                    print('DM SERIES')
+                    diagnosis_details_dm = Diagnosis.objects.filter(patient_uuid=patient_uuid,
+                    ndc_id=cd.dm, server_created_on__date=cd.visit_date).exists()
+                    if not diagnosis_details_dm:
+                        diagnosis_dm_obj = Diagnosis.objects.create(uuid=uuid_id,
+                        user_uuid=user_uuid, patient_uuid=patient_uuid, ndc_id=cd.dm, 
+                        source_treatment=cd.source_treatment, detected_by=cd.detected_by_dm, 
+                        years=cd.detected_since_dm)
+                        diagnosis_dm_obj.server_created_on=cd.visit_date
+                        diagnosis_dm_obj.save()
+                        print('DM SERIES')
                     
             except Patients.DoesNotExist:
                 patients_ids = None
