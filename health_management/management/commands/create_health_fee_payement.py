ad0711c0a3e624bab91b2a836dae521ee9f0b831
loga.vanan <loga.vanan@mahiti.org>
2023-07-05 18:33:27 +0530

    created mangement comment

--- health_management/management/commands/create_health_fee_payement.py
+++ health_management/management/commands/create_health_fee_payement.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from health_management.models import *
 import uuid
+import datetime
 
 class Command(BaseCommand):
     help = "Patient to village profile created"
@@ -23,12 +24,42 @@ class Command(BaseCommand):
                 patient_uuid = patients_ids.uuid
                 health_details = Health.objects.filter(patient_uuid=patient_uuid).exists()
                 if not health_details:
+                    if cd.htn == 9:
+                        ht = 2
+                    elif cd.htn == 7:
+                        ht = 1
+                    else:
+                        ht = ''
+                    if cd.dm == 8:
+                        dm = 2
+                    elif cd.dm == 5:
+                        dm = 1
+                    else:
+                        dm = ''
+                    if cd.detected_since_dm:
+                        day =cd.detected_since_dm.split('-')[0]
+                        month = cd.detected_since_dm.split('-')[1]
+                        year = cd.detected_since_dm.split('-')[2]
+                        dm_years = datetime.date(int(year), int(month), int(day))
+                    else:
+                        dm_years =''
+                    if cd.detected_since_htn:
+                        day =cd.detected_since_htn.split('-')[0]
+                        month = cd.detected_since_htn.split('-')[1]
+                        year = cd.detected_since_htn.split('-')[2]
+                        ht_years = datetime.date(int(year), int(month), int(day))
+                    else:
+                        ht_years = ''
+
+                        
+
                     Health.objects.create(uuid=uuid_id, user_uuid=user_uuid, 
-                    patient_uuid=patient_uuid, is_alcoholic=cd.alcohol, is_tobacco=cd.tobacco, is_smoker=cd.smoking,
-                    dm_check=2,ht_check=2,dm_status=, ht_status=,dm_source_treatment=,ht_source_treatment=,
-                    dm_years=cd.detected_since_dm,ht_years=cd.detected_since_htn,dm_detected_by=,ht_detected_by=)
+                    patient_uuid=patient_uuid, is_alcoholic=cd.alcohol or 0, is_tobacco=cd.tobacco or 0, is_smoker=cd.smoking or 0,
+                    dm_check=2,ht_check=2,dm_status=dm or 0, ht_status=ht or 0, dm_source_treatment=cd.source_treatment or 0, 
+                    ht_source_treatment=cd.source_treatment  or 0,hyper_diabetic=cd.family_history or 0,
+                    ht_years=ht_years or None,dm_years=dm_years or None,
+                    dm_detected_by=cd.detected_by_dm or 0,ht_detected_by=cd.detected_by_htn or 0)
             except Patients.DoesNotExist:
                 patients_ids = None
-                print(';;;;')
                         
         print('Clinic data imported is done')
